0135162222de7481785f4543a20e00e943f4eb28
tomasrpita <tomasrpita@gmail.com>
2024-07-04 05:46:26 +0200

    chore: Update base.html to include latest posts in the blog homepage

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Post
 
 register = template.Library()
@@ -9,3 +11,16 @@ def total_posts():
     total = Post.published.count()
     print("total_posts", total)
     return Post.published.count()
+
+
+@register.inclusion_tag("blog/post/latest_posts.html")
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count("comments")).order_by(
+        "-total_comments"
+    )[:count]
