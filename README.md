20e4c945a2f1a6a2aa69f44fbc9d31b643b0eea8
tomasrpita <tomasrpita@gmail.com>
2024-07-05 08:54:14 +0200

    feat: Add search functionality to the blog

--- README.md
+++ README.md
@@ -16,4 +16,3 @@ Este capítulo explora cómo integrar aplicaciones de terceros. Te guiará a tra
 docker run --name=blog_db -e POSRGRES_DB=blog -e POSTGRES_USER=blog -e POSTGRES_PASSWORD=xxxxx -p 5432:5432 -d postgres:16.2
 ```
 
-```bash
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -8,7 +9,7 @@ from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 
 
@@ -126,3 +127,21 @@ def post_comment(request, post_id):
         comment.save()
 
     return render(request, "blog/post/comment.html", {"post": post, "comment": comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+    return render(
+        request,
+        "blog/post/search.html",
+        {"form": form, "query": query, "results": results},
+    )
